2e066cb88c7d6ccc25ef3835f4f94bb79af34134
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-06 21:48:42 +0900

    feat: 초안 작성

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -46,7 +46,24 @@ function Contact() {
           <h1>이 세희</h1>
           <ul className="contact-list">{infoList}</ul>
           <p id="introduce">
-            탄탄한 기본기를 중요시하는 <b>백엔드 개발자</b>를 꿈꾸고 있습니다.
+            <span>
+              탄탄한 기본기를 중요시하는 <b>백엔드 개발자</b>가 되기 위해
+              노력하고 있습니다.
+            </span>
+            <ol id="myRule">
+              <li>
+                작은 기술이더라도 정확하게 알고 사용하기 위해 <b>공식 문서</b>나{' '}
+                <b>소스 코드</b>를 먼저 찾아봅니다.
+              </li>
+              <li>
+                개발자의 입장이 아닌 <b>사용자의 입장</b>을 생각하며 서비스를
+                개발합니다.
+              </li>
+              <li>
+                <b>활발한 커뮤니케이션</b>을 지향하며 팀원들과 적극적으로
+                소통합니다.
+              </li>
+            </ol>
           </p>
           <ul className="skill-list">{skillList}</ul>
         </div>
